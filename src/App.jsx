e4205924593d7b8238d3e70448ff8a06a7bb1217
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-02-20 17:28:49 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Board from "./components/Board.jsx"
+import "./styles/App.scss"
+
+function App() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Board />
+        </div>
+    )
+}
+
+export default App
